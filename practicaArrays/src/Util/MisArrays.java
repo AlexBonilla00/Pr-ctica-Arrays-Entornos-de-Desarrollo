876882e1cde6b3d1f278c79f405c10e3b66a00ce
AlexBonilla00 <abm0056@alu.medac.es>
2024-05-07 17:22:49 +0200

    He corregido que en el junit haya beforeall y e añadido la mediana

--- practicaArrays/src/Util/MisArrays.java
+++ practicaArrays/src/Util/MisArrays.java
@@ -1,5 +1,7 @@
 package Util;
 
+import java.util.Arrays;
+
 public abstract class MisArrays {
 /**
  * Calcula la supuesta media de los numeros entre 0 y 10
@@ -20,4 +22,32 @@ public abstract class MisArrays {
 		}
 		return (float) suma / array.length;
 	}
+	
+	/**
+	 * Calcula mediana del array de numeros
+	 * 
+	 * param num entre 0 y 10
+	 * @return La mediana del array
+	 * @throws IllegalArgumentException si el array no esta entre 0 y 10
+	 */
+	public static float medianaNotas(int[] array) {
+	for(int i=0; i<array.length; i++) {
+		if(array[i]<0 || array[i]>10) {
+			throw new IllegalArgumentException("Debe ser los numeros entre 0 y 10");
+		}
+		}
+	Arrays.sort(array);
+	int medio = array.length/2;
+	if( array.length % 2 ==1) {
+		return array[medio];
+		
+	}
+	else {
+		return (float) (array[medio-1]+array[medio])/2;
+	}
+	}
+	
+
+	
 }
+
